db83933c8c2d5e91b8fbd3769b5ec36862b174f1
Joshua Grondziowski <jlg224@pitt.edu>
2025-03-21 12:21:40 -0400

    Added geting list of teams and single teams based on name and id Also added ability to delete teams based on ID

---
+++ API/DTOs/TeamDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.DTOs
+{
+    public class TeamDTO
+    {
+        public int Team_ID { get; set; }
+        public string Team_Name { get; set; }
+        public string Team_City { get; set; }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,22 @@
 using Microsoft.EntityFrameworkCore;
-using WebApplication1.Database;
+using WebApplication1.Database; // Make sure this is the correct namespace
+using Microsoft.OpenApi.Models; // Add this using statement
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>  // Use options
+{
+    options.SwaggerDoc("v1", new OpenApiInfo // Add this block
+    {
+        Title = "Your API Title", // Customize
+        Version = "v1",
+        Description = "API Description" // Customize
+    });
+});
 
 // Register DbContext
 builder.Services.AddDbContext<GOBContext>(options =>
